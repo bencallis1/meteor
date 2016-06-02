5e3bedf9ebd9722671c25a1b5db4f87bacb79e4d
bencallis1 <bencallis1@gmail.com>
2016-06-01 20:45:05 -0600

    enabled 3rd party auth with facebook

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -39,7 +39,7 @@ export default class Task extends Component {
                 />
 
                 <span className="text">
-                    <strong>{this.props.task.username}</strong>
+                    <strong>{this.props.task.username}</strong> :
                     {this.props.task.text}
                 </span>
             </li>

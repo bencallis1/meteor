c2c2ac6b92d57233ad6bff47f1270fce556e17e9
bencallis1 <bencallis1@gmail.com>
2016-06-01 19:05:30 -0600

    Modified the App component to get data from a mongo collection. Also added propTypes on the App component

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// This will create the mongo collection for us and get everything wired up
+import '../imports/api/tasks.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
